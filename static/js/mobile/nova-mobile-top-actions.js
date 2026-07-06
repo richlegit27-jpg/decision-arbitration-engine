36290a1c8e6a5eaf6c31b8fe41a1de2114e2dbc8
richard <richlegit27@gmail.com>
2026-07-06 14:53:32 -0700

    Extract mobile top actions ownership

---
+++ static/js/mobile/nova-mobile-top-actions.js
@@ -0,0 +1,38 @@
+(() => {
+    const TAG = "[NOVA_MOBILE_ONE_ROW_NEW_SESSIONS_COPY_REGEN_V10_CONSOLIDATED_20260625]";
+
+    // PARK_IDS
+
+    // setImportant()
+
+    // parkOldButton()
+
+    // ensureRow()
+
+    // setSessionId()
+
+    // escapeHtml()
+
+    // cleanText()
+
+    // openNew()
+
+    // openSessions()
+
+    // runCopy()
+
+    // runRegen()
+
+    // runCapturedRowAction()
+
+    // bindButton()
+
+    // unparkRealHeaderButtons()
+
+    // apply()
+
+
+    window.NovaMobileTopRowV10Apply20260625 = apply;
+    window.NovaMobileTopRowV10OpenSessions20260625 = openSessions;
+
+})();
